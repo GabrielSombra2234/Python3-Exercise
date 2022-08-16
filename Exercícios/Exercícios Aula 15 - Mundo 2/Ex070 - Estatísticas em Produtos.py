44d88c7a90e84f06a14f70e691316e6080d80d6a
GabrielSombra2234 <gsantos152@outlook.com.br>
2022-08-16 16:55:46 -0300

    Exercício 70 pronto!

--- Exercícios/Exercícios Aula 15 - Mundo 2/Ex070 - Estatísticas em Produtos.py
+++ Exercícios/Exercícios Aula 15 - Mundo 2/Ex070 - Estatísticas em Produtos.py
@@ -7,7 +7,6 @@
 
 # Módulos
 from time import sleep
-import math
 
 # Apresentação
 print('=' * 51)
@@ -22,17 +21,38 @@ print('=' * 51)
 sleep(0.7)
 
 # Váriaveis
+totalCompra = acimaDeMil = cont = menorPreco = 0
+menorProduto = ''
+
 # While e Lógica
 while True:
-    # Carrinho de compras
-    produtoComprado = str(input('Digite o nome do produto comprado: ')).capitalize()
-    precoProduto = int(input('Digite o preço do produto comprado: R$'))
-    condicaoContinuar = str(input('Adicionar mais produtos ao carrinho? [S/N]: ')).upper()
     
-    # Colocar mais compras no carrinho
-    if condicaoContinuar == 'S':
-        produtoComprado = str(input('Digite o nome do produto comprado: ')).capitalize()
-        precoProduto = int(input('Digite o preço do produto comprado: R$'))
+    # Informações do produto
+    nomeProduto = str(input('Digite o nome do produto: ')).capitalize()
+    precoProduto = float(input('Digite o preço do produto: R$'))
+    cont += 1
+    
+    # Preço total e Verificação
+    totalCompra += precoProduto
+    if precoProduto > 1000:
+        acimaDeMil += 1
+    
+    # Produto mais barato e Verificação
+    if cont == 1 or precoProduto < menorPreco:
+        menorPreco = precoProduto
+        menorProduto = nomeProduto
 
+    # Loop sim ou não
+    resposta = ' '
+    while resposta not in 'SN':
+        print('=' * 51)
+        resposta = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
+    if resposta == 'N':
+        break
 
-# Finalização 
+# Finalização
+print('=' * 51)
+print(f'O total da compra foi de R${totalCompra:.2f}!')
+print(f'Temos {acimaDeMil} produtos acima de R$1000.00!')
+print(f'O produto mais barato foi {menorProduto}, custando R${menorPreco:.2f}!')
+print('-FIM-')
